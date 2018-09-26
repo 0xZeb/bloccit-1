3f01c3121e8acb8e372b74151bb783c11b9efadd
Lexi Zgud <lexibitesblog@gmail.com>
2018-09-25 21:16:32 -0500

    Checkpoint Topic Resource 2 Submitted

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -13,5 +13,61 @@ module.exports = {
             .catch((err) => {
                 callback(err);
             })
+    },
+
+    addTopic(newTopic, callback) {
+        return Topics.create({
+            title: newTopic.title,
+            description: newTopic.description
+        })
+            .then((topic) => {
+                callback(null, topic);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+    },
+
+    getTopic(id, callback) {
+        return Topics.findById(id)
+            .then((topic) => {
+                callback(null, topic);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+    },
+
+    deleteTopic(id, callback) {
+        return Topics.destroy({
+            where: { id }
+        })
+            .then((topic) => {
+                callback(null, topic);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+    },
+
+    updateTopic(id, updatedTopic, callback) {
+        return Topics.findById(id)
+            .then((topic) => {
+                if (!topic) {
+                    return callback("Topic not found");
+                }
+
+                //#1
+                topic.update(updatedTopic, {
+                    fields: Object.keys(updatedTopic)
+                })
+                    .then(() => {
+                        callback(null, topic);
+                    })
+                    .catch((err) => {
+                        callback(err);
+                    });
+            });
     }
+
 }
