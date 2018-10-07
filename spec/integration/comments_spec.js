b4a84ccbf7e12e81d38a0eb2d8dec59f1198d84c
Lexi Zgud <lexibitesblog@gmail.com>
2018-10-06 23:59:58 -0500

    Comments Assignment - submitted

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -142,6 +142,41 @@ describe("routes : comments", () => {
                             });
                     })
             });
+
+            it("should not delete another user's comment", (done) => {
+                User.create({
+                    email: "david@davidbowie.com",
+                    password: "123456789"
+                })
+                .then((user) => {
+                    expect(user.email).toBe("david@davidbowie.com");
+                    expect(user.id).toBe(2);
+                    request.get({
+                        url: "http://localhost:3000/auth/fake",
+                        form: {
+                            role: "member",
+                            userId: user.id
+                        }
+                    }, (err, res, body) => {
+                        done();
+                    });
+                    Comment.all()
+                    .then((comments) => {
+                        const commentCountBeforeDelete = comments.length;
+                        expect(commentCountBeforeDelete).toBe(1);
+                        request.post(
+                            `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+                                (err, res, body) => {
+                                    Comment.all() 
+                                    .then((comments) => {
+                                        expect(err).toBeNull();
+                                        expect(comments.length).toBe(commentCountBeforeDelete);
+                                        done();
+                                    })
+                                });
+                    });
+                });
+            });
         });
     });
 
@@ -222,4 +257,40 @@ describe("routes : comments", () => {
         });
 
     }); //end context for signed in user
+
+    describe("admin attempting to perform CRUD actions for Comment", () => {
+        beforeEach((done) => {
+            request.get({
+                    url: "http://localhost:3000/auth/fake",
+                    form: {
+                        role: "admin",
+                        userId: this.user.id
+                    }
+                },
+                (err, res, body) => {
+                    done();
+                }
+            );
+        });
+        describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
+            it("should delete the comment with the associated ID", (done) => {
+                Comment.all()
+                    .then((comments) => {
+                        const commentCountBeforeDelete = comments.length;
+                        expect(commentCountBeforeDelete).toBe(1);
+                        request.post(
+                            `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+                            (err, res, body) => {
+                                expect(res.statusCode).toBe(302);
+                                Comment.all()
+                                    .then((comments) => {
+                                        expect(err).toBeNull();
+                                        expect(comments.length).toBe(commentCountBeforeDelete - 1);
+                                        done();
+                                    })
+                            });
+                    });
+            });
+        });
+    });
 });
